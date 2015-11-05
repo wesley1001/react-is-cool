f7ae0edf0c92ea4e4f76edcb274b1de0ad198592
ytll21 <ytll21@gmail.com>
2015-11-05 11:45:47 +0800

    test goodrain

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'React is Cool!' });
 });
 
 module.exports = router;

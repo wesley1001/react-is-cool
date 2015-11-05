4de1c18c07f15869220d6cccb1b52f05a81aa44d
ytll21 <ytll21@gmail.com>
2015-11-05 21:25:46 +0800

    use mongoose instead of mongodb native, and update test case

--- tests/mongodb.spec.js
+++ tests/mongodb.spec.js
@@ -1,9 +1,20 @@
 describe("MongoDB", function() {
     it("is there a server running", function(next) {
-        var MongoClient = require('mongodb').MongoClient;
-        MongoClient.connect('mongodb://127.0.0.1:27017/fastdelivery', function(err, db) {
-            expect(err).toBe(null);
+
+        var mongoose = require('mongoose');
+
+        mongoose.connect('mongodb://127.0.0.1:27017/reactiscool')
+
+        var db = mongoose.connection;
+
+        db.on('error', function(err) {
+            expect("open").toBe(err);
             next();
+        });
+
+        db.once('open', function (callback) {
+            expect(true).toBe(true);
+            next()
         });
     });
 });

d31697c098ab7fdc43884d4af6a7bf51640354dd
ytll21 <ytll21@gmail.com>
2015-11-07 01:33:12 +0800

    add controller test case

--- views/Base.js
+++ views/Base.js
@@ -1,3 +1,19 @@
-/**
- * Created by kevin on 11/5/15.
- */
+module.exports = function(response, template) {
+    this.response = response;
+    this.template = template;
+};
+module.exports.prototype = {
+    extend: function(properties) {
+        var Child = module.exports;
+        Child.prototype = module.exports.prototype;
+        for(var key in properties) {
+            Child.prototype[key] = properties[key];
+        }
+        return Child;
+    },
+    render: function(data) {
+        if(this.response && this.template) {
+            this.response.render(this.template, data);
+        }
+    }
+}
